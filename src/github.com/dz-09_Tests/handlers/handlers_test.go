941882bf6325d0d15e2cb268e37394cc20d4195f
kazanskii.da@phystech.edu <kazanskii.da@phystech.edu>
2024-04-01 20:49:16 +0300

    test refactoring

--- src/github.com/dz-09_Tests/handlers/handlers_test.go
+++ src/github.com/dz-09_Tests/handlers/handlers_test.go
@@ -10,10 +10,17 @@ func TestaddTransaction(t *testing.T) {
 	// Setup
 	parsedTime := time.Now()
 	// Тестовые данные, которые ожидаем добавить в Базу данных
+	// Commission = Amount * 0.02 - "transfer" & Currency in [ "USD" "EUR" "GBP" "JPY"]
+	// Commission = Amount * 0.05 - "transfer" & Currency in [ "RUB" ]
+	// Commission = 0 otherwise
 	testData := []models.Transactions{
 		{ID: 1, UserID: 1, Amount: 100.00, Currency: "USD", TransactionType: "transfer", Category: "SBP", TransactionDate: parsedTime, Description: "Service 01", Commission: 2.00},
 		{ID: 2, UserID: 1, Amount: 200.00, Currency: "EUR", TransactionType: "transfer", Category: "SBP", TransactionDate: parsedTime, Description: "Service 02", Commission: 4.00},
 		{ID: 3, UserID: 1, Amount: 1000.00, Currency: "GBP", TransactionType: "transfer", Category: "SBP", TransactionDate: parsedTime, Description: "Service 02", Commission: 20.0},
+		{ID: 4, UserID: 3, Amount: 1e20, Currency: "GBP", TransactionType: "transfer", Category: "SBP", TransactionDate: parsedTime, Description: "Service 01", Commission: 2e18},
+		{ID: 5, UserID: 3, Amount: 100, Currency: "RUB", TransactionType: "transfer", Category: "SBP", TransactionDate: parsedTime, Description: "Service 04", Commission: 5.},
+		{ID: 5, UserID: 3, Amount: 100, Currency: "RUB", TransactionType: "purchase", Category: "FOOOOOOOOdd", TransactionDate: parsedTime, Description: "Заказ", Commission: 0.},
+		{ID: 5, UserID: 3, Amount: 100, Currency: "RUB", TransactionType: "nock-nock", Category: "FOOd", TransactionDate: parsedTime, Description: "Заказ", Commission: 0.},
 	}
 	// Execute & Verify
 	//Проверяем Расчет комиссии и обновление щтампа времени транзакции
